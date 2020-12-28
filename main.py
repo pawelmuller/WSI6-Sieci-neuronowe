24a10759ac8518ebaf488e5fe882eb42c6b42af1
Paweł <pawelmuller42@gmail.com>
2020-12-28 22:21:50 +0100

    Added methods allowing data import.

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+from struct import unpack
+from array import array
+import numpy as np
+
+TEST_IMAGES_PATH = 'Data/t10k-images-idx3-ubyte'
+TEST_LABELS_PATH = 'Data/t10k-labels-idx1-ubyte'
+TRAIN_IMAGES_PATH = 'Data/train-images-idx3-ubyte'
+TRAIN_LABELS_PATH = 'Data/train-labels-idx1-ubyte'
+
+IMAGES_OFFSET = 16
+LABELS_OFFSET = 8
+
+
+def import_data():
+    train_labels = load_labels(TRAIN_LABELS_PATH, 2049)
+    train_images = load_images(TRAIN_IMAGES_PATH, 2051)
+    test_labels = load_labels(TEST_LABELS_PATH, 2049)
+    test_images = load_images(TEST_IMAGES_PATH, 2051)
+    return (train_images, train_labels), (test_images, test_labels)
+
+
+def load_images(filename, correct_magic_number):
+    with open(filename, 'rb') as file:
+        magic_number, number_of_images, image_height, image_width = unpack(">IIII", file.read(IMAGES_OFFSET))
+        if magic_number != correct_magic_number:
+            raise ValueError(f'Incorrect magic number. Expected {correct_magic_number}, got {magic_number}.')
+        data = array("B", file.read())
+
+        images = []
+        for i in range(number_of_images):
+            image = np.array(data[i * image_height * image_width:(i + 1) * image_height * image_width])
+            image = image.reshape(image_height, image_width)
+            images.append(image)
+    return images
+
+
+def load_labels(filename, correct_magic_number):
+    with open(filename, 'rb') as file:
+        magic_number, number_of_items = unpack(">II", file.read(LABELS_OFFSET))
+        if magic_number != correct_magic_number:
+            raise ValueError(f'Incorrect magic number. Expected {correct_magic_number}, got {magic_number}.')
+        labels = array("B", file.read())
+    return labels
+
+
 def main():
-    pass
+    train_data, test_data = import_data()
 
 
 if __name__ == '__main__':
